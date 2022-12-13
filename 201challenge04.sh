5252ac41275c3e40bfa719e3bd26d2bb61d1c20f
Andrew <asiterlet.as@outlook.com>
2022-12-12 18:37:55 -0600

    201 Ops Challange 04

---
+++ 201challenge04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Andrew Siterlet
+# Date of latest revision: 12/12/22
+mkdir dir{1..4}
+dirs=("dir1" "dir2" "dir3" "dir4")
+touch ${dirs[0]}/text.txt
+touch ${dirs[1]}/text.txt
+touch ${dirs[2]}/text.txt
+touch ${dirs[3]}/text.txt
+
+# Main
+
+# End
