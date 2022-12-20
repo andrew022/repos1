a00dfcd6cbadb01ab9297e35bf81fec83881e51f
Andrew Siterlet <asiterlet.as@outlook.com>
2022-12-19 19:56:39 -0600

    07

--- 201challenge07.sh
+++ 201challenge07.sh
@@ -1,2 +1,4 @@
 sudo lshw -c cpu | grep -E 'product:|vendor:|physical id:|bus info:|width:'
 lshw -c memory | grep -E 'description:|physical id:|size:' | head -n3
+sudo lshw -c display | grep -E 'description:|product:|vendor:|physical id:|bus info:|width:|clock:|capabilities:|configuration:|resources:'
+sudo lshw -c network | grep -E 'description:|product:|vendor:|physical id:|bus info:|logical name:|version:|serial:|size:|capacity:|width:|cock:|capabilities:|configuration:|resources:'
