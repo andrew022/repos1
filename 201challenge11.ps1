4d2a0d147fbeb211391e29ebf89aa043a701f740
Andrew Siterlet <asiterlet.as@outlook.com>
2023-01-03 18:32:00 -0600

    Challenge 11

---
+++ 201challenge11.ps1
@@ -0,0 +1,8 @@
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+#IPv4
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+#IPv6
+netsh advfirewall firewall add rule name="ICMP Allow incoming V6 echo request" protocol="icmpv6:8,any" dir=in action=allow
+Enable-PSRemoting -Force
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
